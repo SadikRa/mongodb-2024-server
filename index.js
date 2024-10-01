381b5d6ba47e4c9c49ac9ca18360b31e52a8fa87
SadikRa <sadikrahman494@gmail.com>
2024-10-01 22:18:10 +0600

    first post added done

--- index.js
+++ index.js
@@ -25,8 +25,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const userCollection = client.db('usersDB2024').collection('users')
+
+    app.post("/users", (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const result = userCollection.insertOne(user)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
