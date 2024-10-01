44a029be2c45ff4e740d2fedcb59cfc48c9a9e9d
SadikRa <sadikrahman494@gmail.com>
2024-10-01 20:08:23 +0600

    env added done

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000 ;
+
+
+app.use(cors()) ;
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
